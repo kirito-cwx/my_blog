b17b3a9832e0b28b6a847d72298aca4a31d8ce6a
chenwanxin <1349687131@qq.com>
2023-07-11 00:48:50 +0800

    添加 bootstrip布局

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render_to_response, get_object_or_404
-from .models import Blog,Blog_Type
+from .models import Blog, Blog_Type
 
 
 # Create your views here.
 def blog_list(request):
     blogs = Blog.objects.all()
-    context = {"blogs": blogs}
+    blog_types = Blog_Type.objects.all()
+    context = {"blogs": blogs,
+               "blog_types": blog_types}
     return render_to_response("blog/blog_list.html", context=context)
 
 
@@ -14,10 +16,13 @@ def blog_detail(request, blog_id):
     context = {"blog": blog}
     return render_to_response("blog/blog_detail.html", context=context)
 
-def blog_detail_type_name(request,blog_type_id):
-    blog_type = get_object_or_404(Blog_Type,pk=blog_type_id)
+
+def blog_detail_type_name(request, blog_type_id):
+    blog_type = get_object_or_404(Blog_Type, pk=blog_type_id)
+    blog_types = Blog_Type.objects.all()
     blogs = Blog.objects.filter(blog_type=blog_type)
     context = {"blogs": blogs,
-               "blog_type": blog_type}
+               "blog_type": blog_type,
+               "blog_types": blog_types}
 
     return render_to_response("blog/blog_detail_type_name.html", context=context)
